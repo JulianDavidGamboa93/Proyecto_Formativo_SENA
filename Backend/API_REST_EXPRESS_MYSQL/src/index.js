27765291a827928f5d599fcfcfa2ea234d04f6f4
Julian David Gamboa <Zychki@gmail.com>
2023-09-06 14:06:48 -0500

    .

--- Backend/API_REST_EXPRESS_MYSQL/src/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-const main = () => {
-    app.listen(app.get("port"));
-    console.log(`Server on port ${app.get("port")}`);
-};
-
-main();
--- Backend/API_REST_EXPRESS_MYSQL/src/routes/ecommerce.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const routers=Router();
-
-routers.get("/", (request, response) => {
-    res.send("Jdgamboa");
-})
-
-export default router;
